57c7224e120e71c02aafabed17b89b754b66c965
aceerez <asafepl@gmail.com>
2021-01-05 10:07:50 +0200

    Change encryption from mongoose encrypt to HASH encryption.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 
 const app = express();
@@ -26,10 +26,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password']
-});
+
 
 
 const User = new mongoose.model("User", userSchema);
@@ -50,7 +47,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err) {
     if (err) {
@@ -63,8 +60,8 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
-  console.log(username);
+  const password = md5(req.body.password);
+
   User.findOne({
     email: username
   }, function(err, foundUser) {
